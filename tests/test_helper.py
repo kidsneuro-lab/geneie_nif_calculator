a92d14dd04d629ff6f0138e18ad8641d987f33e0
h-joshi <hihi.joshi@gmail.com>
2024-09-10 08:09:46 +0000

    Fixed calculation of Acceptor NIF table

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -14,20 +14,20 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 class TestNIFCalculation:
-    def test_calc_nif_known_seq(self, motif_stats):
+    def test_calc_nif_known_donor_seq(self, donor_motif_stats):
         sequence = 'ATCGGTGCAAGT'
         expected_result = {'U5': {
                                 'E4_D5': {
                                     'seq': 'ATCGGTGCA',
-                                    'pctl': 0.01,
-                                    'freq': 10
+                                    'pctl': 0.0,
+                                    'freq': 0
                                 }
                             },
                             'U1': {
                                 'E3_D6': {
                                     'seq': 'TCGGTGCAA',
-                                    'pctl': 0.33,
-                                    'freq': 20
+                                    'pctl': 0.0,
+                                    'freq': 0
                                 },
                                 'E2_D7': {
                                     'seq': 'CGGTGCAAG',
@@ -38,11 +38,33 @@ class TestNIFCalculation:
                             'U6': {
                                 'E1_D8': {
                                     'seq': 'GGTGCAAGT',
-                                    'pctl': 0.0,
-                                    'freq': 0
+                                    'pctl': 0.0193762132371837,
+                                    'freq': 2
                                 }
                             }}
-        result = NIFAnnotator.calc_nif(sequence, 'donor', motif_stats)
+        result = NIFAnnotator.calc_nif(sequence, 'donor', donor_motif_stats)
+        assert result == expected_result
+        
+    def test_calc_nif_known_acceptor_seq(self, acceptor_motif_stats):
+        sequence = 'AATTGTAAGAA'
+        expected_result = {'ACC': {
+                                'A9_A1': {
+                                    'seq': 'AATTGTAAG',
+                                    'pctl': 0.0437847502076319,
+                                    'freq': 4
+                                },
+                                'A8_E1': {
+                                    'seq': 'ATTGTAAGA',
+                                    'pctl': 0.0125765578525753,
+                                    'freq': 1
+                                },
+                                'A7_E2': {
+                                    'seq': 'TTGTAAGAA',
+                                    'pctl': 0.0459776698592195,
+                                    'freq': 4
+                                }
+                            }}
+        result = NIFAnnotator.calc_nif(sequence, 'acceptor', acceptor_motif_stats)
         assert result == expected_result
 
 class TestSequenceExtraction:
